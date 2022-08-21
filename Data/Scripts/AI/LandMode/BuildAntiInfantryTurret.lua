c0bf78185db044fbdf707cd835312c3f1e660950
Connor Hess <20891893+AlyMar1994@users.noreply.github.com>
2022-08-21 18:30:02 -0400

    CLEAN: BuildAntiInfantryTurret.lua
    - CLEAN: Cleaning files of trailing spaces/no trailing newline (VSC AUTO)

--- Data/Scripts/AI/LandMode/BuildAntiInfantryTurret.lua
+++ Data/Scripts/AI/LandMode/BuildAntiInfantryTurret.lua
@@ -44,11 +44,11 @@ require("pgevents")
 -- Build a single turret.
 
 function Definitions()
-	
+
 	Category = "Build_AntiInfantry_Turret"
 	TaskForce = {
 	{
-		"MainForce"					
+		"MainForce"
 		,"TaskForceRequired"
 		,"UC_Empire_Buildable_Anti_Infantry_Turret | UC_Rebel_Buildable_Anti_Infantry_Turret | UC_Pirate_Buildable_Anti_Infantry_Turret | UC_Hutt_Rapid_Fire_Laser_Turret = 1"
 	}
@@ -58,7 +58,7 @@ end
 
 function MainForce_Thread()
 	--MessageBox("%s -- Building a turret.", tostring(Script))
-		
+
 	-- Make sure we've ended up with a build location that's reasonably close to our original target
 	pad_table = MainForce.Get_Reserved_Build_Pads()
 	for i,pad in pad_table do
@@ -66,12 +66,10 @@ function MainForce_Thread()
 			ScriptExit()
 		end
 	end
-	
+
 	-- Build the task force
-	-- Blocking shouldn't be necessary, but we'll use it to ease watching the script	
+	-- Blocking shouldn't be necessary, but we'll use it to ease watching the script
 	MainForce.Set_Plan_Result(true)
 	BlockOnCommand(MainForce.Build_All())
 	ScriptExit()
 end
-
-
