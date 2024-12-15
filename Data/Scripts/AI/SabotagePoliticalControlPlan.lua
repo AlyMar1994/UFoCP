01484b3a45afe03bf37a77dca38d8277e371d71a
Connor Hess <20891893+AlyMar1994@users.noreply.github.com>
2024-12-15 16:39:24 -0500

    fix: disable SabotagPoliticalControlPlan to prevent useless bounty hunter buying
    Addresses #416

--- Data/Scripts/AI/SabotagePoliticalControlPlan.lua
+++ Data/Scripts/AI/SabotagePoliticalControlPlan.lua
@@ -41,31 +41,41 @@
 
 require("pgevents")
 
-function Definitions()
-	DebugMessage("%s -- In Definitions", tostring(Script))
+-- AM1994 12/15/2024 - Disable this plan as a part of UFoCP #408.
+-- Because political control/political fighting was cut from the
+-- game, Bounty Hunters no longer affect political control.  This
+-- plan forces the AI to over-buy useless BHs that aren't apart
+-- of DeployUnitOnTarget, and sit on planets.
+-- Prefer the AI spend money more wisely on smugglers or other
+-- forms of unit expansion.
 
-	IgnoreTarget = true;
-	Category = "Weaken_Planet"
+function Definitions()
+	--DebugMessage("%s -- In Definitions", tostring(Script))
+
+	IgnoreTarget = true
+	--Category = "Weaken_Planet"
+	Category = "AlwaysOff"
 	TaskForce = {
 	{
 		"BountyForce"
-		,"Bounty_Hunter_Team_E | Bounty_Hunter_Team_R = 1"
+		--,"Bounty_Hunter_Team_E | Bounty_Hunter_Team_R = 1"
 	}
 	}
 
-	DebugMessage("%s -- Done Definitions", tostring(Script))
+	--DebugMessage("%s -- Done Definitions", tostring(Script))
 end
 
 function BountyForce_Thread()
-	DebugMessage("%s -- In BountyForce_Thread.", tostring(Script))
-	Sleep(1)
+	--DebugMessage("%s -- In BountyForce_Thread.", tostring(Script))
+	--Sleep(1)
 
-	AssembleForce(BountyForce)
-	Sleep(1)
-	BlockOnCommand(BountyForce.Move_To(Target))
-	BountyForce.Set_Plan_Result(true)
-	LandUnits(BountyForce)
-	DebugMessage("%s -- BountyForce Done!  Exiting Script!", tostring(Script))
+	--AssembleForce(BountyForce)
+	--Sleep(1)
+	--BlockOnCommand(BountyForce.Move_To(Target))
+	--BountyForce.Set_Plan_Result(true)
+	--LandUnits(BountyForce)
+	--DebugMessage("%s -- BountyForce Done!  Exiting Script!", tostring(Script))
+	DebugMessage("%s -- Script disabled!  Exiting Script!", tostring(Script))
 	ScriptExit()
 end
 
