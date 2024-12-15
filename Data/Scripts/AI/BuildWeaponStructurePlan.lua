c690628012d1b6d88069323dbf9aa173b0394fda
Connor Hess <20891893+AlyMar1994@users.noreply.github.com>
2024-12-15 08:36:08 -0500

    git: clean BuildWeaponStructurePlan.lua
    - CLEAN: Cleaning files of trailing spaces/no trailing newline (VSC AUTO)

--- Data/Scripts/AI/BuildWeaponStructurePlan.lua
+++ Data/Scripts/AI/BuildWeaponStructurePlan.lua
@@ -44,7 +44,7 @@ require("pgevents")
 
 function Definitions()
 	DebugMessage("%s -- In Definitions", tostring(Script))
-	
+
 	Category = "Build_Weapon"
 	IgnoreTarget = true
 	TaskForce = {
@@ -59,12 +59,12 @@ end
 
 function StructureForce_Thread()
 	DebugMessage("%s -- In StructureForce_Thread.", tostring(Script))
-	
+
 	Sleep(1)
-	
+
 	StructureForce.Set_As_Goal_System_Removable(false)
 	AssembleForce(StructureForce)
-	
+
 	StructureForce.Set_Plan_Result(true)
 	DebugMessage("%s -- StructureForce done!", tostring(Script));
 	ScriptExit()
@@ -74,8 +74,3 @@ function StructureForce_Production_Failed(tf, failed_object_type)
 	DebugMessage("%s -- Abandonning plan owing to production failure.", tostring(Script))
 	ScriptExit()
 end
-
-
-
-
-
