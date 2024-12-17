20c783b07df7644a53343925426237b58b08b998
Connor Hess <20891893+AlyMar1994@users.noreply.github.com>
2024-12-17 17:19:07 -0500

    fix: bounty hunters not activating ability after enabling
    Addresses #408

--- Data/Scripts/AI/DeployUnitOnTarget.lua
+++ Data/Scripts/AI/DeployUnitOnTarget.lua
@@ -49,7 +49,7 @@ function Definitions()
 	IgnoreTarget = true
 	TaskForce = {
 	{
-		"MainForce",
+		"BountyForce",
 		"DenyHeroAttach",
 		"Bounty_Hunter_Team_E | Bounty_Hunter_Team_R = 1"
 	}
@@ -57,17 +57,40 @@ function Definitions()
 
 end
 
-function MainForce_Thread()
-	DebugMessage("%s -- In MainForce_Thread.", tostring(Script))
+function BountyForce_Thread()
+	DebugMessage("%s -- In BountyForce_Thread.", tostring(Script))
 
-	BlockOnCommand(MainForce.Produce_Force(AITarget))
+	if EvaluatePerception("Needs_Smuggler_Removed", PlayerObject, AITarget) < 1.0 then
+		DebugMessage("%s -- Unable to find target for BountyForce!", tostring(Script))
+		ScriptExit()
+	end
+
+	--Goal is global so there's no staging area for AssembleForce, but since we're using a stealth unit we can stage at the target
+	BlockOnCommand(BountyForce.Produce_Force(AITarget))
 
 	-- Landing a hero deploys it, removing it from the game and killing the script.  So,
 	-- we have to indicate success before we land the unit, even though she hasn't deployed.
 	-- If a hero killer gets her before she deploys, the plan should die before setting itself successful.
-	MainForce.Set_Plan_Result(true)
+	BountyForce.Set_Plan_Result(true)
 
-	BlockOnCommand(LandUnits(MainForce))
+	-- AM1994 12/17/2024 - Use Activate_Ability() instead of LandUnits.
+	-- When trying to land a bounty hunter, the AI will instead rapidly
+	-- land and send the BH to space without actually activating ability
+	-- and prematurely complete the plan.  I don't know why landing a BH
+	-- doesn't act like other abilities, but this seems to work.
+	BountyForce.Activate_Ability()
+	--BlockOnCommand(LandUnits(BountyForce))
 
-	DebugMessage("%s -- Finished MainForce_Thread.", tostring(Script))
+	DebugMessage("%s -- Finished BountyForce_Thread.", tostring(Script))
+	ScriptExit()
+end
+
+function BountyForce_No_Units_Remaining()
+	DebugMessage("%s -- Prematurely lost BountyForce!  Exiting...", tostring(Script))
+	ScriptExit()
+end
+
+function BountyForce_Production_Failed(tf, failed_object_type)
+	DebugMessage("%s -- Abandonning plan owing to production failure.", tostring(Script))
+	ScriptExit()
 end
