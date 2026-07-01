678b2dc80c0acc302880dfaff8dbb39ec5fd44b1
James Suzuki <20891893+AlyMar1994@users.noreply.github.com>
2026-07-01 13:22:07 -0400

    chore: clean AI_Plan_ExpansionGeneric_DefendSpaceStation
    - CLEAN: Cleaning files of trailing spaces/no trailing newline (VSC AUTO)

--- Data/Scripts/AI/SpaceMode/AI_Plan_ExpansionGeneric_DefendSpaceStation.lua
+++ Data/Scripts/AI/SpaceMode/AI_Plan_ExpansionGeneric_DefendSpaceStation.lua
@@ -42,46 +42,43 @@
 require("pgevents")
 
 function Definitions()
-	
 	AllowEngagedUnits = true
 	IgnoreTarget = true
 	Category = "Defend_Space_Station"
-	TaskForce = {
+	TaskForce =
 	{
-		"MainForce"
-		, "TaskForceRequired"
-		, "DenySpecialWeaponAttach"
+		{
+			"MainForce",
+			"TaskForceRequired",
+			"DenySpecialWeaponAttach"
+		}
 	}
-	}
-
 end
 
 function MainForce_Thread()
+	local focus_fire_on_target = Find_Nearest(Target, "Frigate | Capital", PlayerObject, false)
 
-	focus_fire_on_target = Find_Nearest(Target, "Frigate | Capital", PlayerObject, false)
-	
 	while TestValid(focus_fire_on_target) do
 		-- Cancel all goals
 		Purge_Goals(PlayerObject)
-		
+
 		Sleep(1)
-		
+
 		-- Use all idle units, mapwide
-		MainForce.Collect_All_Free_Units()	
-		
+		MainForce.Collect_All_Free_Units()
+
 		while TestValid(focus_fire_on_target) do
 			MainForce.Collect_All_Free_Units()
 			BlockOnCommand(MainForce.Attack_Target(focus_fire_on_target), 5)
 		end
-	
+
 		Sleep(1)
 		MainForce.Set_Plan_Result(true)
-		
+
 		focus_fire_on_target = Find_Nearest(Target, "Frigate | Capital", PlayerObject, false)
-	
 	end
 end
 
 function MainForce_Unit_Damaged(tf, unit, attacker, deliberate)
-	--Override self preservation behavior, just save the station!
+	-- Override self preservation behavior, just save the station!
 end
