4ac53378c666ed00ea7049c3f0761e5763b39c13
James Suzuki <20891893+AlyMar1994@users.noreply.github.com>
2026-09-14 10:38:02 -0400

    chore: clean BurnUnits.lua
    - CLEAN: Cleaning files of trailing spaces/no trailing newline (VSC AUTO)

--- Data/Scripts/AI/SpaceMode/BurnUnits.lua
+++ Data/Scripts/AI/SpaceMode/BurnUnits.lua
@@ -39,30 +39,28 @@
 --
 --/////////////////////////////////////////////////////////////////////////////////////////////////
 
-require("pgevents")
+require("PGEvents")
 
 function Definitions()
-	
+	Category = "Burn_Units_Space"
+	TaskForce =
+	{
+		{
+			"MainForce",
+			"TaskForceRequired",
+			"DenySpecialWeaponAttach"
+		}
+	}
 	AllowEngagedUnits = true
 	IgnoreTarget = true
-	Category = "Burn_Units_Space"
-	TaskForce = {
-	{
-		"MainForce"
-		, "TaskForceRequired"
-		, "DenySpecialWeaponAttach"
-	}
-	}
-
 end
 
 function MainForce_Thread()
-
 	-- Cheat to wrap the game up if this is a firesale
 	if (EvaluatePerception("Should_Firesale_Space", PlayerObject) > 0) then
 		reveal_ai = FogOfWar.Reveal_All(PlayerObject)
 		MainForce.Set_As_Goal_System_Removable(false)
-		
+
 		-- We're now also revealing FOW here for the human player (was previously handled by a hard-coded system)
 		-- Make sure that there isn't a scripted scenario underway that doesn't want automatic FOW reveals
 		if Is_Multiplayer_Mode() == false and ((EvaluatePerception("Is_Skirmish_Mode", PlayerObject) == 1) or
@@ -73,43 +71,41 @@ function MainForce_Thread()
 
 	-- Do this at least once (it may just be an attempt to burn off extra units, rather than a firesale)
 	repeat
-	
 		-- Cancel all goals
 		Purge_Goals(PlayerObject)
-		
+
 		Sleep(1)
-		
+
 		-- Use all idle units, mapwide
-		MainForce.Collect_All_Free_Units()	
-	
+		MainForce.Collect_All_Free_Units()
+
 		target = Find_Nearest(MainForce, "Structure | Capital", PlayerObject, false)
 		if target == nil then
 			target = Find_Nearest(MainForce, PlayerObject, false)
-		end	
-	
+		end
+
 		while TestValid(target) do
 			DebugMessage("%s-- collecting all free units and attacking target:%s", tostring(Script), tostring(target))
 			MainForce.Collect_All_Free_Units()
 			BlockOnCommand(MainForce.Attack_Move(target), 20)
 		end
-	
+
 		Sleep(5)
 		MainForce.Set_Plan_Result(true)
-	
+
 	-- If this is a firesale condition, continue to burn off units
 	-- This must be done within the same plan because we don't want to destroy the
 	-- reveal objects turning FOW back on (which would result in the FOW being toggled repeatedly).
 	until (EvaluatePerception("Should_Firesale_Space", PlayerObject) == 0)
-	
-	MainForce.Release_Forces(1.0)	
-	
+
+	MainForce.Release_Forces(1.0)
+
 	Sleep(30)
 end
 
 -- Override default event to prevent plan from ending under firesale conditions
 -- We want the fog of war reveal for the player to persist.
 function MainForce_No_Units_Remaining()
-
 end
 
 function MainForce_Unit_Damaged(tf, unit, attacker, deliberate)
